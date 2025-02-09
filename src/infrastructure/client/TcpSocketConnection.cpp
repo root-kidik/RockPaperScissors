4c388afbbebb001fe9c335fb0e9cea0c17c0e0b3
root-kidik <rtkid.nik@gmail.com>
2025-02-10 00:38:32 +0700

    fix: connection

--- src/infrastructure/client/TcpSocketConnection.cpp
+++ src/infrastructure/client/TcpSocketConnection.cpp
@@ -18,7 +18,7 @@ void TcpSocketConnection::send(std::string&& data)
 {
     assert(m_tcp_socket && "Connection is failed!");
 
-    if (!m_tcp_socket)
+    if (!m_tcp_socket || m_tcp_socket->state() != QAbstractSocket::ConnectedState)
         return;
 
     m_tcp_socket->write(data.c_str());
